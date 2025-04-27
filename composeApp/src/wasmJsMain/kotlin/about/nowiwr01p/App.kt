2a935749d20f96250d4924a49b01b6a3201bf537
Andrey Larionov <nowiwr01p@icloud.com>
2025-04-27 16:05:49 +0400

    fixes

--- composeApp/src/wasmJsMain/kotlin/about/nowiwr01p/App.kt
+++ composeApp/src/wasmJsMain/kotlin/about/nowiwr01p/App.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
@@ -37,7 +36,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import kotlinx.browser.window
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun App() {
